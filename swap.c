8f5f6952ea3e53f69ceafa2da5d6fcfe78d37058
RangiraDave <rangiradev666@gmail.com>
2023-12-18 08:16:45 +0200

    SOrting algorithms

---
+++ swap.c
@@ -0,0 +1,18 @@
+#include "sort.h"
+
+/**
+ * swap - Function to swap two elements.
+ * @a: First element.
+ * @b: second element.
+ * Return: Nothing.
+ */
+
+void swap_int(int a, int b)
+{
+	int temp;
+
+	temp = a;
+	a = b;
+	b = temp;
+	temp = 0;
+}
